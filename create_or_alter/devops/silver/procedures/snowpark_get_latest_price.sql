5a6a3ebc545459517640b58a20f0e700d6ed1ba9
Jeff Hollan <jeff.hollan@snowflake.com>
2024-02-22 09:39:58 -0800

    fix handler

--- create_or_alter/devops/silver/procedures/snowpark_get_latest_price.sql
+++ create_or_alter/devops/silver/procedures/snowpark_get_latest_price.sql
@@ -4,5 +4,5 @@ returns text
 language python
 runtime_version = '3.10'
 packages = ('snowflake-snowpark-python')
-handler = 'get_latest_price'
+handler = 'snowpark_get_latest_price.get_latest_price'
 imports = ('{{ git_base }}/create_or_alter/devops/silver/procedures/snowpark/get_latest_price/snowpark_get_latest_price.py')
