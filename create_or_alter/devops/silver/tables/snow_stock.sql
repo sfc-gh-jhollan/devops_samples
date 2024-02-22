1bb677ea902662b2047cbfd80466a23cca68a8b7
Jeff Hollan <jeff.hollan@snowflake.com>
2024-02-21 22:43:54 -0800

    fix dynamic table from

--- create_or_alter/devops/silver/tables/snow_stock.sql
+++ create_or_alter/devops/silver/tables/snow_stock.sql
@@ -3,7 +3,7 @@ CREATE OR REPLACE DYNAMIC TABLE {{ snow_stock_name }}
 TARGET_LAG = '1 hour'
 WAREHOUSE = {{ dynamic_warehouse }}
 AS
-select * from financial__economic_essentials.cybersyn.stock_price_timeseries
+select * from bronze.stock_price_timeseries
 where 
     ticker = 'SNOW' 
     and variable in ('pre-market_open', 'post-market_close')
