c136ab3b21cfe28ed9d18fbb86e7c688f7ca0e3c
Jeff Hollan <jeff.hollan@snowflake.com>
2024-02-21 22:28:18 -0800

    adding schema

--- create_or_alter/devops/base.sql
+++ create_or_alter/devops/base.sql
@@ -1,4 +1,9 @@
 --!jinja
 
+create schema if not exists bronze;
+use schema bronze;
 EXECUTE IMMEDIATE FROM './bronze/tables/stock_price_timeseries.sql';
+
+create schema if not exists silver;
+use schema silver;
 EXECUTE IMMEDIATE FROM './silver/tables/snow_stock.sql' using ( dynamic_warehouse => '{{ dynamic_warehouse }}', snow_stock_name => '{{ snow_stock_name }}' );
