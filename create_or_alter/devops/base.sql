b07b580196c78d4b1f36387e4b345ebb52bc36f6
Jeff Hollan <jeff.hollan@snowflake.com>
2024-02-21 22:22:22 -0800

    testing nested v2

--- create_or_alter/devops/base.sql
+++ create_or_alter/devops/base.sql
@@ -1,4 +1,4 @@
 --!jinja
 
 EXECUTE IMMEDIATE FROM './bronze/tables/stock_price_timeseries.sql';
-EXECUTE IMMEDIATE FROM './silver/tables/snow_stock' using ( dynamic_warehouse => '{{ dynamic_warehouse }}', snow_stock_name => '{{ snow_stock_name }}' );
+EXECUTE IMMEDIATE FROM './silver/tables/snow_stock' using ( dynamic_warehouse => {{ dynamic_warehouse }}, snow_stock_name => {{ snow_stock_name }} );
