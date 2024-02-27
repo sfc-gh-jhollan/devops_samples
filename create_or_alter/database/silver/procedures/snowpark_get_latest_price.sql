ecfe0d929d8b00b7c4d436f8f8e125b4fcae64d1
Jeff Hollan <jeff.hollan@snowflake.com>
2024-02-27 15:54:34 +0100

    folder fix

--- create_or_alter/database/silver/procedures/snowpark_get_latest_price.sql
+++ create_or_alter/database/silver/procedures/snowpark_get_latest_price.sql
@@ -5,4 +5,4 @@ language python
 runtime_version = '3.10'
 packages = ('snowflake-snowpark-python')
 handler = 'snowpark_get_latest_price.get_latest_price'
-imports = ('{{ git_base }}/create_or_alter/devops/silver/procedures/snowpark/get_latest_price/snowpark_get_latest_price.py')
+imports = ('{{ git_base }}/create_or_alter/database/silver/procedures/snowpark/get_latest_price/snowpark_get_latest_price.py')
